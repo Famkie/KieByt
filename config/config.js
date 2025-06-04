dabcf4728eb6ad14a756224a371d33e4d4ff7672
Famkie <famkie.id@gmail.com>
2025-06-04 11:33:08 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export default {
+const config = {
   token: process.env.TOKEN,
   clientId: process.env.CLIENT_ID,
   guildId: process.env.GUILD_ID,
-  prefix: process.env.PREFIX || '!',  // tambahkan prefix di sini
+  prefix: process.env.PREFIX || '!',
 };
+
+export default config;
