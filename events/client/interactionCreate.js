685884c722ff4a1ba845d4010ba1d09f1882394b
Famkie <famkie.id@gmail.com>
2025-06-04 11:23:00 +0700

    Create interactionCreate.js

---
+++ events/client/interactionCreate.js
@@ -0,0 +1,16 @@
+export default {
+  name: 'interactionCreate',
+  async execute(interaction, client) {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.slashCommands.get(interaction.commandName);
+    if (!command) return;
+
+    try {
+      await command.execute(interaction, client);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: '❌ Error executing command.', ephemeral: true });
+    }
+  },
+};
