dd18712e8439752d07e5f7fe5e43173cfe402dbe
Famkie <famkie.id@gmail.com>
2025-06-04 11:24:48 +0700

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+export default {
+  token: process.env.TOKEN,
+  clientId: process.env.CLIENT_ID,
+  guildId: process.env.GUILD_ID,
+  prefix: process.env.PREFIX || '!',  // tambahkan prefix di sini
+};
